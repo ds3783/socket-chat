93e0ffd710fb345556cac5754e58f9389f4575d6
ds3783@gmail.com <ds3783@gmail.com@6a6d080a-6d32-11de-9c28-0bda66f61e62>
2012-11-29 04:49:58 +0000

    catch a Exception

--- src/net/ds3783/chatserver/communicate/delivery/MessageProcessorEventImpl.java
+++ src/net/ds3783/chatserver/communicate/delivery/MessageProcessorEventImpl.java
@@ -1,8 +1,11 @@
 package net.ds3783.chatserver.communicate.delivery;
 
 
+import com.google.gson.Gson;
 import net.ds3783.chatserver.communicate.ContextHelper;
 import net.ds3783.chatserver.messages.Message;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,6 +15,8 @@ import net.ds3783.chatserver.messages.Message;
  * 通过Event模式实现消息的处理
  */
 public class MessageProcessorEventImpl implements MessageProcessor {
+    private static Log logger = LogFactory.getLog(MessageProcessorEventImpl.class);
+    private Gson gson;
     private MessageDispatcher messageDispatcher;
     private ContextHelper contextHelper;
 
@@ -20,8 +25,13 @@ public class MessageProcessorEventImpl implements MessageProcessor {
         Event evt = new Event();
         evt.setName(msg.getType());
         evt.setMessage(msg);
-        messageDispatcher.dispatchEvent(evt);
-        contextHelper.forget(msg);
+        try {
+            messageDispatcher.dispatchEvent(evt);
+        } catch (Exception e) {
+            logger.error("error occored during dispatchEvent:" + gson.toJson(evt) + "\r\n original Message is :" + gson.toJson(msg) + "\r\n errMsg:" + e.getMessage(), e);
+        } finally {
+            contextHelper.forget(msg);
+        }
 
     }
 
@@ -31,5 +41,9 @@ public class MessageProcessorEventImpl implements MessageProcessor {
 
     public void setContextHelper(ContextHelper contextHelper) {
         this.contextHelper = contextHelper;
+    }
+
+    public void setGson(Gson gson) {
+        this.gson = gson;
     }
 }
